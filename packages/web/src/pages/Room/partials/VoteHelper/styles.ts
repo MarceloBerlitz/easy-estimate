5763cc63fd2dfbc5d79499fb1eb3bc321bd60689
Marcelo Berlitz <x475682@gruposantander.com>
2024-02-21 10:32:52 -0300

    smartphone fix

--- packages/web/src/pages/Room/partials/VoteHelper/styles.ts
+++ packages/web/src/pages/Room/partials/VoteHelper/styles.ts
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 export const VotesHelperWrapper = styled.div`
   display: flex;
   flex-direction: row;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
   gap: 1rem;
 `;
 
