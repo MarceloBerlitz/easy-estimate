6e1498b98a5936df19536eb3341a76e12fd79438
Marcelo Berlitz <x475682@gruposantander.com>
2024-02-21 13:19:08 -0300

    Translates tooltips

--- packages/lib/src/enums/vote-parameters.enum.ts
+++ packages/lib/src/enums/vote-parameters.enum.ts
@@ -7,7 +7,7 @@ export enum VoteParametersEnum {
 export const VOTE_PARAMETERS_OPTIONS = Object.values(VoteParametersEnum);
 
 export const VOTE_PARAMETERS_TOOLTIPS = {
-  [VoteParametersEnum.COMPLEXITY]: 'Dificuldade, interação com outros times, manutenibilidade.',
-  [VoteParametersEnum.EFFORT]: 'Volume de trabalho, número de componentes envolvidos.',
-  [VoteParametersEnum.RISK]: 'Dúvidas, incertezas técnicas ou de negócio.',
+  [VoteParametersEnum.COMPLEXITY]: 'Difficulty, interaction with other teams, maintainability.',
+  [VoteParametersEnum.EFFORT]: 'Volume of work, number of components involved.',
+  [VoteParametersEnum.RISK]: 'Doubts, technical or business uncertainties.',
 };
