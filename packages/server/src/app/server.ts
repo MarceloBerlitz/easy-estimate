c91e620e72f4060f65247a4451aa221d56a48f95
MarceloBerlitz <x475682@gruposantander.com>
2024-03-01 17:03:38 -0300

    Fix public path

--- packages/server/src/app/server.ts
+++ packages/server/src/app/server.ts
@@ -1,23 +1,20 @@
 import express from 'express';
 import http from 'http';
+import path from 'path';
 import * as dotenv from 'dotenv';
-
-import { EnvironmentHelper } from '../helpers/environment.helper';
 
 export class Server {
   private server: http.Server;
 
   public constructor() {
-    dotenv.config({ path: `${__dirname}/../.env` });
+    dotenv.config({ path: '../../.env' });
 
     const app = express();
     this.server = http.createServer(app);
 
-    if (!EnvironmentHelper.isLocal()) {
-      const baseDir = `${__dirname}/public/`;
-      app.use(express.static(baseDir));
-      app.get('*', (_, res) => res.sendFile('index.html', { root: baseDir }));
-    }
+    const baseDir = path.join(__dirname, '../public');
+    app.use(express.static(baseDir));
+    app.get('*', (_, res) => res.sendFile('index.html', { root: baseDir }));
   }
 
   public get instance(): http.Server {
