56de27e86db2e4d7289f92617973c14ff9efde9b
MarceloBerlitz <x475682@gruposantander.com>
2024-03-11 21:28:54 -0300

    Continues visual improvements

--- packages/web/src/pages/Room/partials/SubHeader/styles.ts
+++ packages/web/src/pages/Room/partials/SubHeader/styles.ts
@@ -5,9 +5,18 @@ export const SubHeaderContainer = styled.div`
   flex-direction: row;
   justify-content: space-between;
   margin: 1rem 0;
+  gap: 1rem;
 
   > * {
     margin: 0;
     width: fit-content;
+  }
+
+  @media (max-width: 700px) {
+    flex-direction: column-reverse;
+    align-items: center;
+    /* > div {
+      align-self: end;
+    } */
   }
 `;
